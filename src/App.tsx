4294a0c1904f8e2e9d94aa7ca41722d66a49e40c
CHIRAG B CHAVDA <chiragchavda0036@gmail.com>
2022-07-06 12:30:42 +0530

    Done staging

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./utils/theme";
+import "./assets/css/style.css";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MainNavigation from "./components/MainNavigation";
+import { AuthWrapper } from "./context/auth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<React.Suspense fallback={<></>}>
+				<BrowserRouter>
+					<AuthWrapper>
+							<div className="wrapper">
+								<Header />
+								<main>
+									<MainNavigation />
+								</main>
+								<Footer />
+							</div>
+					</AuthWrapper>
+					<ToastContainer />
+				</BrowserRouter>
+			</React.Suspense>
+		</ThemeProvider>
+	);
+};
 
 export default App;
